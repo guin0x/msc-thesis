92a08000428f46301540b344ba78090c3307e0ee
Guilherme Alvarenga <35092729+guin0x@users.noreply.github.com>
2025-05-22 20:42:29 +0200

    added statistics of b0 since cant store everything

--- scale_dependent_analysis.py
+++ scale_dependent_analysis.py
@@ -74,6 +74,7 @@ def process_radial_wind(record):
                 'b0': result['b0'],
                 'b1': result['b1'],
                 'b2': result['b2'],
+                'phi_bin_centers': result['phi_bin_centers']
             }
         return None
     except Exception as e:
