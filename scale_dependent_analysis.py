d31a30e9a0627351f355418d8bc152880baf5997
Guilherme Alvarenga <35092729+guin0x@users.noreply.github.com>
2025-05-25 10:41:58 +0200

    included wind_field_median to be returned as well

--- scale_dependent_analysis.py
+++ scale_dependent_analysis.py
@@ -74,6 +74,7 @@ def process_radial_wind(record):
                 'b0': result['b0'],
                 'b1': result['b1'],
                 'b2': result['b2'],
+                'wind_field_median': result['wind_field_mean']
             }
         return None
     except Exception as e:
